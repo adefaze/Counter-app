039c2b78b86b02931481a373160edba88904acd9
Abiodun Adefila <adefaze@gmail.com>
2023-03-02 12:37:07 +0100

    fixes

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+createApp({
+    data() {
+        return {
+            pageTitle: "Project Title",
+            content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis placeat commodi rem veniam laudantium facilis
+      aliquam delectus deleniti molestias incidunt! Neque a et illo vitae.Debitis fuga voluptatum blanditiis assumenda?`
+        };
+
+    }
+}).mount('#app')
