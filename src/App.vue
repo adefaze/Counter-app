205d0383fbd3a77a2619891beadb1128f57013aa
Abiodun Adefila <adefaze@gmail.com>
2023-03-02 13:59:35 +0100

    first build

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,36 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+
+<script setup> 
+  import {ref} from "vue";
+
+  const count = ref(0);
+
+
 </script>
 
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div>
+      <h2>The count is...</h2>
+      <p>{{count}}</p>
+      <button @click="count++">+</button>
+      <button @click="count--">-</button>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100wh;
+  height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+div {
+  text-align: center;
 }
 </style>
+
--- src/main.js
+++ src/main.js
@@ -3,13 +3,4 @@ import App from './App.vue'
 
 import './assets/main.css'
 
-createApp({
-    data() {
-        return {
-            pageTitle: "Project Title",
-            content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis placeat commodi rem veniam laudantium facilis
-      aliquam delectus deleniti molestias incidunt! Neque a et illo vitae.Debitis fuga voluptatum blanditiis assumenda?`
-        };
-
-    }
-}).mount('#app')
+createApp(App).mount('#app')
